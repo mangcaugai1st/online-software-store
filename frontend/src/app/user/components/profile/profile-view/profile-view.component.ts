9c2a7401c71577345df6abfb381211db33dc3fe4
mangcaugai1st <mangcaugai1st@gmail.com>
2024-11-28 18:26:11 +0700

    Add frontend

---
+++ frontend/src/app/user/components/profile/profile-view/profile-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-view.component.html',
+  styleUrl: './profile-view.component.css'
+})
+export class ProfileViewComponent {
+
+}
