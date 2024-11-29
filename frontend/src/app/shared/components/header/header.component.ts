f45cdea78151efcb65f6dffef5da037ed6a21983
mangcaugai1st <mangcaugai1st@gmail.com>
2024-11-29 22:57:57 +0700

    29-Nov-2024

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  bannerPath: string = '/assets/images/banner/Group_1.png';
 }
