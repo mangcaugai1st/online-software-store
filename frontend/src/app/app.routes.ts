b9dc9013dd7cd2d399a43dffcdb423309776c0a7
mangcaugai1st <mangcaugai1st@gmail.com>
2024-12-11 08:36:37 +0700

    Get products by category slug name route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './user/modules/home/home.component';
 import {LoginComponent} from './user/modules/login/login.component';
+import {ProductsComponent} from './user/shared/components/products/products.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: ':slug', component: ProductsComponent },
 ];
