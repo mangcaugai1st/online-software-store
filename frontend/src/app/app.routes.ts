e559a0f5fa5af9d7ee12e6cab7fc9378851cf88d
mangcaugai1st <mangcaugai1st@gmail.com>
2024-12-12 19:20:42 +0700

    Update routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './user/modules/home/home.component';
 import {LoginComponent} from './user/modules/login/login.component';
 import {ProductsComponent} from './user/shared/components/products/products.component';
+import {DetailProductComponent} from './user/shared/components/detail-product/detail-product.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: ':slug', component: ProductsComponent },
+  { path: ':categorySlug', component: ProductsComponent },
+  { path: 'product/:productSlug', component: DetailProductComponent },
 ];
