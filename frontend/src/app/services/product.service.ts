eb9148aa3cd99146b2e2a21d65ce22670adc9b62
mangcaugai1st <mangcaugai1st@gmail.com>
2024-12-12 22:10:36 +0700

    Update product service

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,6 +1,5 @@
-import {Inject, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {environment} from '../../environments/environment';
-import {Category} from '../models/category.model'
 import {HttpClient} from '@angular/common/http';
 import {Product} from '../models/product.model';
 
@@ -11,8 +10,18 @@ export class ProductService {
   private apiUrl = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
+    getProducts()
+    {
+      return this.http.get<Product[]>(`${this.apiUrl}products`);
+    }
+
     getProductsByCategory(categorySlug: string)
     {
       return this.http.get<Product[]>(`${this.apiUrl}categories/category/${categorySlug}`);
+    }
+
+    getProductBySlugName(productSlug: string)
+    {
+      return this.http.get<Product[]>(`${this.apiUrl}products/product/${productSlug}`);
     }
 }
