f26dbbc8cd9464efe99560b70df388fdb4b97dd6
mangcaugai1st <mangcaugai1st@gmail.com>
2024-12-13 22:57:30 +0700

    Update list of products

---
+++ frontend/src/app/user/modules/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {CategoriesComponent} from "../../shared/components/categories/categories.component";
+import {NgForOf} from "@angular/common";
+import {ProductsComponent} from '../../shared/components/products/products.component';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [
+    CategoriesComponent,
+    NgForOf,
+    ProductsComponent,
+    RouterOutlet
+  ],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+}
