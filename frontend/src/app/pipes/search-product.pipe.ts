2424cd07a05daa8308b685e39bf6765f4e3307c2
mangcaugai1st <mangcaugai1st@gmail.com>
2024-12-14 23:07:42 +0700

    Create product search pipe

---
+++ frontend/src/app/pipes/search-product.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from '../models/product.model'
+@Pipe({
+  name: 'searchProduct',
+  standalone: true
+})
+export class SearchProductPipe implements PipeTransform {
+
+  transform(products: Product[], searchText: string){
+    // if (!searchText || !products) {
+    //   return null;
+    // }
+
+    return products.filter(product =>
+      product.name?.toLowerCase().includes(searchText.toLowerCase())
+    );
+  }
+}
