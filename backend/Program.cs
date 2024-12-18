a6b0fe9f9dee32c72a92f0a0f56944a04709c04d
mangcaugai1st <mangcaugai1st@gmail.com>
2024-12-18 22:15:14 +0700

    Authentication and Authorization

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,19 +1,53 @@
+using System.Text;
 using Microsoft.EntityFrameworkCore;
 using backend.Models;
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.IdentityModel.Tokens;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// IConfiguration configuration = builder.Configuration;
 
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 // Register the database context
 // DB context must be registered with the dependency injection (DI) container. The container provides the service to controllers.
-
 builder.Services.AddDbContext<MyDbContext>(opt=>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("MyDB")));
+
+// Cấu hình Authentication
+// var tokenConfig = Configuration.GetSection("TokenConfig").Get<TokenConfig>();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            // ValidAudience = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+// Cấu hình Authorization 
+builder.Services.AddAuthorization(Options =>
+{
+    Options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
+    Options.AddPolicy("UserPolicy", policy => policy.RequireRole("User"));
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -30,7 +64,12 @@ if (app.Environment.IsDevelopment())
 // Enable CORS
 app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); 
+
+// Authentication -> Authorization
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.UseAuthorization();
 
