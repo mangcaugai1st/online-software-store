408d88648a8b2ae0b39dc727ab897a7544a94033
mangcaugai1st <mangcaugai1st@gmail.com>
2024-12-22 23:36:58 +0700

    Create user's service

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
+
+  getUsers()
+  {
+    return this.http.get<User[]>(`${this.apiUrl}users`);
+  }
+
+  getUserById(id: string)
+  {
+    return this.http.get<User[]>(`${this.apiUrl}users/${id}`);
+  }
+
+}
