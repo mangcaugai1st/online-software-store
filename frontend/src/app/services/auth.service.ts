4eea18b1a1c13495102c840715a33e5357064a18
mangcaugai1st <mangcaugai1st@gmail.com>
2024-12-26 17:35:54 +0700

    Modify auth service

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,26 +1,83 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment'
+import {HttpClient} from '@angular/common/http';
+import {User} from '../models/user.model'
+import {BehaviorSubject, map, Observable, tap} from 'rxjs';
+import {jwtDecode, JwtPayload} from 'jwt-decode'
+
+interface LoginResponse {
+  token: string;
+  //username: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private isAuthenticated: boolean = false;
+  private apiUrl = environment.apiUrl;
+  currentUser?: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  login(username: string, password: string): boolean {
-    if (username === 'admin' && password === 'password') {
-      this.isAuthenticated = true;
-      return true;
-    }
+  registerHandler() {
+    // return this.http.post(this.apiUrl + 'auth' + '/register', JSON.stringify({}))
+    return this.http.post<User>(`${this.apiUrl}auth/register`, JSON.stringify({}))
+  }
+
+  // Xử lý đăng nhập và lưu trữ thông tin sau khi đăng nhập thành công.
+  loginHandler(credentials: {username: string, password: string}): Observable<LoginResponse>
+  {
+    return this.http.post<LoginResponse>(`${this.apiUrl}auth/login`,credentials).pipe(
+      // tap operator để lưu token
+      tap(response => {
+        // Lưu token
+        localStorage.setItem('token', response.token);
+        if (response.token)
+        {
+          const decodedToken: any = jwtDecode<JwtPayload>(response.token); // returns with the JwtPayload type
+          const username = decodedToken.name; // Lấy tên người dùng từ token
+          const userRole: string = decodedToken.role; // Lấy vai trò user
+          // this.currentUser = username;
+          console.log(`Mã token: ${response.token}, tên người dùng là: ${username}, vai trò là: ${userRole}`);
+
+          window.location.reload();
+        }
+      })
+    );
+  }
+
+  // Kiểm tra xem người dùng đã đăng nhập hay chưa
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token'); // Nếu có token trong LocalStorage thì tức là đã đăng nhập
+  }
+
+  isAdmin(): boolean {
+    const token: any  = localStorage.getItem('token');
+    const decodedToken: any = jwtDecode<JwtPayload>(token);
+    const userRole: string = decodedToken.role;
+
+    if (userRole == "True") {return true}
     return false;
   }
 
-  logout(): void {
-    this.isAuthenticated = false;
+  // Xoá toàn bộ thông tin đã đăng nhập
+  logoutHandler() {
+    localStorage.removeItem('token'); // Xoá token
+    window.location.reload();
   }
 
-  getAuthStatus(): boolean {
-    return this.isAuthenticated;
+  // getAccessToken: Lấy token
+
+  // Lấy tên đăng nhập ở thời điểm hiện tại
+  getCurrentUser(): string
+  {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken: any = jwtDecode(token);
+      this.currentUser = decodedToken.name;
+    }
+    return <string>this.currentUser;
   }
+
+  // refreshToken: làm mới token
 }
