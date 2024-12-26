95fc1eda5ca012a0803abe587ba9ec32163ab798
mangcaugai1st <mangcaugai1st@gmail.com>
2024-12-26 17:21:48 +0700

    Modify in app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgxSpinnerComponent, NgxSpinnerService} from 'ngx-spinner';
 import {HeaderComponent} from './user/shared/components/header/header.component';
 import {FooterComponent} from './user/shared/components/footer/footer.component';
+import {AdminHeaderComponent} from './admin/shared/admin-header/admin-header.component';
+// import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {AuthService} from './services/auth.service';
+import {NgIf} from '@angular/common';
+import {UserComponent} from './user/user.component'
+import {AdminComponent} from './admin/admin.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    NgxSpinnerComponent,
+    AdminHeaderComponent,
+    UserComponent,
+    AdminComponent,
+    NgIf,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  isAdmin: boolean = false;
+
+  constructor (
+    private spinner: NgxSpinnerService,
+    private authService: AuthService,
+  ) {}
+  title = 'Nameless';
+  role: string = 'admin';
+
+  ngOnInit() {
+    this.spinner.show();
+
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 1500);
+
+    this.isAdmin = this.authService.isAdmin();
+  }
 }
