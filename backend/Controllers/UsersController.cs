bcfc2bd7da4da743fb4331f404fc0ab25a9c9917
mangcaugai1st <mangcaugai1st@gmail.com>
2025-03-07 06:29:12 +0700

    Add update user's profile in user controller

--- backend/Controllers/UsersController.cs
+++ backend/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using backend.Models;
 using backend.Models.DTOs;
+using backend.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,13 +11,15 @@ namespace backend.Controllers;
 public class UsersController : ControllerBase
 {
    private readonly ApplicationDbContext _context;
+   private readonly IJwtClaimsService _jwtClaimsService;
 
-   public UsersController(ApplicationDbContext context)
+   public UsersController(ApplicationDbContext context, IJwtClaimsService jwtClaimsService)
    {
       _context = context;
+      _jwtClaimsService = jwtClaimsService;
    }
    
-   // GET: api/Users
+   // API trả về toàn bộ thông tin của tất cả người dùng có trong hệ thống
    [HttpGet]
    public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
    {
@@ -29,8 +32,7 @@ public class UsersController : ControllerBase
          Phone = u.Phone,
       }).ToListAsync();
    }
-  
-   // GET: api/Users/{userId}
+   
    [HttpGet("{userId}")]
    public async Task<ActionResult<User>> GetUser(int userId)
    {
@@ -44,7 +46,6 @@ public class UsersController : ControllerBase
       return user;
    }
    
-   // Post: api/Users
    [HttpPost]
    public async Task<ActionResult<User>> CreateUser(User user)
    {
@@ -52,5 +53,55 @@ public class UsersController : ControllerBase
       await _context.SaveChangesAsync();
       
       return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+   }
+   
+   [HttpPut("update")]
+   public async Task<IActionResult> UpdateUserProfile([FromBody] UpdateUserProfileDto? updateUserProfileDTO)
+   {
+      // Token được lấy từ header của http request dưới dạng: Authorization: Bearer <jwt_token>
+      var jwtToken = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+     
+      if (string.IsNullOrEmpty(jwtToken))
+      {
+         return Unauthorized("JWT Token is required"); 
+      }
+
+      try
+      {
+         // Trích xuất UserId từ Claims của Token
+         var userIdFromClaims = _jwtClaimsService.GetUserIdByJwt(jwtToken);
+         int userId = int.Parse(userIdFromClaims);
+         
+         if (userId <= 0 || updateUserProfileDTO == null)
+         { 
+            return BadRequest(); // Trả về lỗi 404
+         }
+         else
+         {
+            // Tìm kiếm người dùng theo userId
+            var user = await _context.Users.FindAsync(userId);
+            
+            // Cập nhật thông tin người dùng
+            if (user != null)
+            {
+               user.FullName = updateUserProfileDTO.FullName;
+               user.Email = updateUserProfileDTO.Email;
+               user.Phone = updateUserProfileDTO.Phone;
+               user.Address = updateUserProfileDTO.Address;
+
+               // Lưu thay đổi vào cơ sở dữ liệu
+               _context.Users.Update(user);
+               await _context.SaveChangesAsync();
+
+               return Ok(user);
+            }
+         }
+      }
+      catch (Exception ex)
+      { 
+         return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+      }
+      // _context.Entry(user).State = EntityState.Modified;
+      return BadRequest();
    }
 }
---
+++ backend/Models/DTOs/UpdateUserProfileDto.cs
@@ -0,0 +1,9 @@
+namespace backend.Models.DTOs;
+
+public class UpdateUserProfileDto
+{
+    public string? FullName { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; } 
+}
